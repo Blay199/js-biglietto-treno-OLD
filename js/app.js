7117864956b64dbfc1a6c08870307ab0a1a8cf94
Blay199 <a.sorrentino03@gmail.com>
2022-07-21 16:16:40 +0200

    NON CAPISCO COSA NON FUNZIONA

--- js/app.js
+++ js/app.js
@@ -1,2 +1,24 @@
-const km = prompt('inserire chilometri da percorrere')
-const età = prompt('Inserire età passeggero')
+const km = parseFloat( prompt('inserire chilometri da percorrere'))
+const age = parseFloat( prompt('Inserire età passeggero'))
+let trainTicket = km * 0.21;
+
+
+
+if (age < 18) {
+    let trainTicket = trainTicket * 0.2;
+} else if (age > 65) {
+    let trainTicket = trainTicket * 0.4;
+} else {
+    trainTicket = trainTicket;
+}
+
+document.getElementById("trip_cost").innerHTML = "Il prezzo del tuo biglietto è: " + trainTicket + " &euro";
+
+
+
+
+
+
+
+// console.log(km, age)
+console.log(trainTicket)
