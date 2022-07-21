0ac0e85f6f69bfed695215824da8c5ede6b6caba
Blay199 <a.sorrentino03@gmail.com>
2022-07-21 16:26:11 +0200

    Corretto

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 const km = parseFloat( prompt('inserire chilometri da percorrere'));
 const age = parseFloat( prompt('Inserire età passeggero'));
+let trainTicket = km * 0.21;    
 
 
 
 if (age < 18) {
-    let trainTicket = trainTicket * 0.2;
+    trainTicket = trainTicket - trainTicket * 0.2;
 } else if (age > 65) {
-    let trainTicket = trainTicket * 0.4;
+    trainTicket = trainTicket - trainTicket * 0.4;
 } else {
-    let trainTicket = km * 0.21;    
+    trainTicket = trainTicket
 }
 
 document.getElementById("trip_cost").innerHTML = "Il prezzo del tuo biglietto è: " + trainTicket + " &euro";
